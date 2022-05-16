75545a372d437a48d126673bdb7641e7c119a646
荣顶 <wangrongding@qq.com>
2022-05-16 19:00:12 +0800

    fix: 添加element-plus样式引入

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index.ts';
 import pinia from './store/index.ts';
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 
