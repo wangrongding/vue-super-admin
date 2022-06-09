53e35358e7cd58c23fbcc9a5405d1cc17b26e660
荣顶 <wangrongding@qq.com>
2022-06-09 15:54:55 +0800

    chore: 修改eslint校验配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,26 @@ module.exports = {
   },
   extends: [
     // 'plugin:vue/essential',// vue核心规范
-    'plugin:vue/vue3-recommended', // vue3规范
     'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended', // vue3规范
     'prettier', // prettier规范,覆盖eslint格式配置,写在最后
     '.eslintrc-auto-import.json',
   ],
+  // 针对特定文件重写eslint规则
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.vue'],
+      rules: {},
+    },
+  ],
+  // 支持ts的最新语法
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
+  // 添加vue和@typescript-eslint插件，增强eslint的能力
   plugins: ['vue', '@typescript-eslint'],
   rules: {
     'import/no-extraneous-dependencies': 0, // 禁止使用无关的包
@@ -24,6 +34,6 @@ module.exports = {
     'vue/multi-word-component-names': 0, // 禁止使用多单词组件名
     'no-unused-vars': 0, // 关闭未使用变量检查
     'no-param-reassign': 0, // 禁止对函数参数进行重新赋值
-    'no-console': 2, //
+    '@typescript-eslint/no-empty-function': 0, // 禁止空函数
   },
-};
+}
